bcf24964fb7ff02b98a134ecf5ededf5514e8ebc
lesotropyx <90562367+lesotropyx@users.noreply.github.com>
2023-05-16 15:16:45 +0300

    16.05 пробелы

--- include/Automata.h
+++ include/Automata.h
@@ -12,7 +12,7 @@ enum class STATES {
 };
 
 class Automata {
-public: 
+public:
     Automata() : cash(0), state(STATES::OFF), indchoose(-1) {}
     void on();
     void off();
@@ -24,7 +24,7 @@ public:
     void cancel();
     void cook();
     void finish();
-private:
+ private:
     int cash;
     int indchoose;
     std::string menu[3] = {"Raf", "Espresso", "Flat-white"};
